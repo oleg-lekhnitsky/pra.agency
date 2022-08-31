a8f66dc0987241b0dc1f298a6941bd0efdc84503
oleg <oleg.lekhnitsky@gmail.com>
2022-08-31 20:50:28 +0300

    form fix

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,7 +1,7 @@
 let form = document.querySelector('form');
 if (form){
   form.addEventListener('submit', function() {
-    window.location.href = '/index.html';
+    window.location.href = '/submit-success.html';
   });
 }
 
